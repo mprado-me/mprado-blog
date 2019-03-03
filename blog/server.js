6cf927575b62179ded336321fb74a2682cc16cf7
Marco Aurélio Prado <marco.pdsv@gmail.com>
2019-03-03 01:54:55 -0300

    Setting dev and prod configs on blog

--- blog/server.js
+++ blog/server.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const next = require('next')
+const nconf = require('nconf');
 
 const dev = process.env.NODE_ENV !== 'prod'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+
+// Setting dev or prod configs
+nconf.env().argv().required(['NODE_ENV']);
+if(nconf.get('NODE_ENV') == 'prod'){
+    nconf.file({file: "configs/prod.json"});
+}
+else if(nconf.get('NODE_ENV') == 'dev') {
+    nconf.file({file: "configs/dev.json"});
+}
+else {
+    throw Error("NODE_ENV deve pertencer a ['dev', 'prod']");
+}
 
 app.prepare()
 .then(() => {
@@ -13,9 +26,9 @@ app.prepare()
     return handle(req, res)
   })
 
-  server.listen(3000, (err) => {
+  server.listen(nconf.get('port'), (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${nconf.get('port')}`)
   })
 })
 .catch((ex) => {
